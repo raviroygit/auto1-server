664bd292123410cc8301e9f281f0f6a106c65bcb
raviroygit <ravi93448@yahoo.com>
2025-02-20 12:11:12 +0530

    function changes

--- src/routes/auto_ai.routes.ts
+++ src/routes/auto_ai.routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+    extractCompanyInformation,
   generateAiResponse,
   generateFormatResponse,
 } from "../controller/auto_ai.controller";
@@ -9,6 +10,6 @@ const routerAutoAi = express.Router();
 routerAutoAi.post("/ai", generateAiResponse);
 routerAutoAi.post("/format", generateFormatResponse);
 
-routerAutoAi.post("/company-info", generateFormatResponse);
+routerAutoAi.post("/company-info", extractCompanyInformation);
 
 export default routerAutoAi;
